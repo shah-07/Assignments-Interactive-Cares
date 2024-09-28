4c7da6704b9f176c382d5d9948fe2b580bb4502d
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-09-29 00:01:52 +0600

    Update index.php

--- banking-app/public/index.php
+++ banking-app/public/index.php
@@ -7,44 +7,17 @@ date_default_timezone_set('Asia/Dhaka');
 
 require '../vendor/autoload.php';
 
-const BASE_PATH = __DIR__ . '/../';
 
+const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'core/functions.php';
+
+
 
 // Create a Router instance
 $router = new Router();
 $router->run();
 
 
-
-
-
-
-
-
-// $router->add('/admin', function() {
-//     if ($_SESSION['user']['role'] === 'Admin') {
-//         $adminController = new AdminController();
-//         $customers = $adminController->viewAllCustomers();
-//         $transactions = $adminController->viewAllTransactions();
-//         include '../views/admin_dashboard.php';
-//     } else {
-//         header('Location: /login');
-//         exit();
-//     }
-// });
-
-// $router->add('/customer', function() {
-//     if ($_SESSION['user']['role'] === 'Customer') {
-//         $customerController = new CustomerController();
-//         $transactions = $customerController->viewTransactions($_SESSION['user']['email']);
-//         $balance = $customerController->getBalance($_SESSION['user']['email']);
-//         include '../views/customer_dashboard.php';
-//     } else {
-//         header('Location: /login');
-//         exit();
-//     }
-// });
 
 // Parse the requested URL
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
